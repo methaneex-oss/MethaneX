cb24547e57a8a92eb3272c01d714d902bf92e9d1
methaneex-oss <methaneex@gmail.com>
2026-08-27 01:20:32 +0100

    Make decision scoring context aware

--- core/cpp/include/jarvis/core/decision.hpp
+++ core/cpp/include/jarvis/core/decision.hpp
@@ -8,7 +8,7 @@ namespace jarvis::core {
 
 class DecisionEngine {
 public:
-    std::vector<Decision> rank(const std::vector<CandidateAction>& actions, double uncertainty) const;
+    std::vector<Decision> rank(const std::vector<CandidateAction>& actions, double uncertainty, double threat = 0.0) const;
 };
 
 } // namespace jarvis::core
