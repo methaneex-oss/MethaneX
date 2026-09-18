8cee1b562623f094595dec406f4b7270abe36c25
methaneex-oss <methaneex@gmail.com>
2026-09-18 21:43:39 +0100

    feat(evolution): connect canary decisions to controller

--- core/cpp/include/jarvis/core/evolution_controller.hpp
+++ core/cpp/include/jarvis/core/evolution_controller.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "jarvis/core/evolution_adoption.hpp"
+#include "jarvis/core/evolution_canary.hpp"
 #include "jarvis/core/evolution_history.hpp"
 
 namespace jarvis::core {
@@ -12,12 +13,18 @@ public:
 
     bool record_evaluation(const EvolutionExperiment& experiment);
     bool adopt(EvolutionExperiment& experiment);
-    bool rollback(const std::string& parameter_key, const std::string& experiment_id);
+    CanaryDecision observe_canary(const std::string& parameter_key,
+                                  const std::string& experiment_id,
+                                  const CanaryObservation& observation);
+    bool rollback(const std::string& parameter_key, const std::string& experiment_id,
+                  const std::string& reason = "manual_rollback",
+                  double observed_delta = 0.0);
 
 private:
     EvolutionModel& model_;
     EvolutionHistory& history_;
     EvolutionSafetyPolicy policy_;
+    EvolutionCanary canary_;
 };
 
 } // namespace jarvis::core
